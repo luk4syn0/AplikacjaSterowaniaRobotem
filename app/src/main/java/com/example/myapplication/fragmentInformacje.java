2c90498c6e3215c7849a1344d7fe4dc387181b0a
lukasz <luszczyk15@icloud.com>
2023-05-21 20:58:23 +0200

    Funkcjonalnosci przyciskow navbar, fragment sterowania

--- app/src/main/java/com/example/myapplication/fragmentInformacje.java
+++ app/src/main/java/com/example/myapplication/fragmentInformacje.java
@@ -1,4 +1,23 @@
 package com.example.myapplication;
 
-public class fragmentInformacje {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class fragmentInformacje extends Fragment {
+
+    public fragmentInformacje() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_informacje, container, false);
+        // Initialize and set up your views here
+
+        return view;
+    }
 }
